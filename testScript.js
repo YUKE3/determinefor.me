9e82660df54ed5db5c74a4700b4056641ab2f6bb
Vincent <yuke@cs.stonybrook.edu>
2024-07-05 08:07:52 -0400

    Proof of concept script

--- testScript.js
+++ testScript.js
@@ -1,17 +1,37 @@
 import { config } from "dotenv";
 import { OpenAI } from "openai";
+import fs from "fs";
 
 config();
 
-console.log(process.env.API_KEY);
+var watermelon;
+const img = fs.readFileSync("watermelon.jpg");
+watermelon = "data:image/jpeg;base64," + Buffer.from(img).toString("base64");
 
 const openai = new OpenAI({
     apiKey: process.env.API_KEY,
 });
 
 const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: "Hello!  " }],
+    model: "gpt-4o",
+    messages: [
+        {
+            role: "user",
+            content: [
+                {
+                    type: "text",
+                    text: "This image contains a few items circled in red with corresponding numbers. Could you pick the best item and give me the number",
+                },
+                {
+                    type: "image_url",
+                    image_url: {
+                        url: watermelon,
+                        detail: "low",
+                    },
+                },
+            ],
+        },
+    ],
 });
 
 console.log(response);
